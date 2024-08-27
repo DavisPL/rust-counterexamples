d9f116be1aaea889eb2c86e4f8918319537b3de7
muhamamd-hassnain <23100250@lums.edu.pk>
2024-08-27 01:44:17 +0000

    :paperclip: cargo clippy

--- src/bin/dangling_nightly_lifetime.rs
+++ src/bin/dangling_nightly_lifetime.rs
@@ -6,32 +6,30 @@ present as of Rust 1.82.0
 https://github.com/rust-lang/rust/issues/120217
 */
 
-// #![feature(arbitrary_self_types)]
+#![feature(arbitrary_self_types)]
 
-// trait Static<'a> {
-//     fn proof(self: *const Self, s: &'a str) -> &'static str;
-// }
+trait Static<'a> {
+    fn proof(self: *const Self, s: &'a str) -> &'static str;
+}
 
-// fn bad_cast<'a>(x: *const dyn Static<'static>) -> *const dyn Static<'a> {
-//     x as _
-// }
+fn bad_cast<'a>(x: *const dyn Static<'static>) -> *const dyn Static<'a> {
+    x as _
+}
 
-// impl Static<'static> for () {
-//     fn proof(self: *const Self, s: &'static str) -> &'static str {
-//         s
-//     }
-// }
+impl Static<'static> for () {
+    fn proof(self: *const Self, s: &'static str) -> &'static str {
+        s
+    }
+}
 
-// fn extend_lifetime(s: &str) -> &'static str {
-//     bad_cast(&()).proof(s)
-// }
+fn extend_lifetime(s: &str) -> &'static str {
+    bad_cast(&()).proof(s)
+}
 
-// fn main() {
-//     let s = String::from("Hello World");
-//     let slice = extend_lifetime(&s);
-//     println!("Now it exists: {slice}");
-//     drop(s);
-//     println!("Now it’s gone: {slice}");
-// }
-
-fn main() {}
+fn main() {
+    let s = String::from("Hello World");
+    let slice = extend_lifetime(&s);
+    println!("Now it exists: {slice}");
+    drop(s);
+    println!("Now it’s gone: {slice}");
+}
